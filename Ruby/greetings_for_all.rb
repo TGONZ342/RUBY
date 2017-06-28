9fff5f7b004d9a019e061648ddb4905adf695659
TGONZ342 <gonz8950@gmail.com>
2017-06-28 15:12:05 -0700

    Update greetings_for_all.rb

--- Ruby/greetings_for_all.rb
+++ Ruby/greetings_for_all.rb
@@ -1,1 +1,12 @@
+def greeting (bloop)
+    if bloop == nil
+        puts "noble stranger"
+    elsif bloop.kind_of?(Integer)
+        puts "Error! That doesn't sound like a name."
+    else
+        puts "HELLO #{bloop}"
+    end
+end
 
+greeting "WORLD"
+greeting " I'M HAPPPY TO BE HERE"
