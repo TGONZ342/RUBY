23a807cc63cda2259e0bd0ec0138f33d45160b9c
TGONZ342 <gonz8950@gmail.com>
2017-06-28 15:14:13 -0700

    Update family_affairs.rb

--- Ruby/family_affairs.rb
+++ Ruby/family_affairs.rb
@@ -1,1 +1,9 @@
+class Object
+    def find_the_gingers(x)
+        x.delete_if {|key, values| values != "red"}
+    end
+end
 
+dupont_family = {:matthew => "red", :mary => "blonde", :virginia => "brown", :gaetan => "red", :fred => "red"}
+
+puts "#{find_the_gingers(dupont_family).keys}"
